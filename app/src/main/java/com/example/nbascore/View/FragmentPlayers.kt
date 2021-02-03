b98784bc95f7c9d1b65c4186447cae7c050e0120
Paweł Pustuła <pawel.pustula98@gmail.com>
2021-02-03 23:54:49 +0100

    wyświetlenie zawodników

--- app/src/main/java/com/example/nbascore/View/FragmentPlayers.kt
+++ app/src/main/java/com/example/nbascore/View/FragmentPlayers.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.nbascore.R
-import kotlinx.android.synthetic.main.fragment_welcome.*
+import com.example.nbascore.ViewModel.PlayerAdapter
+import com.example.nbascore.ViewModel.PlayerViewModel
+import kotlinx.android.synthetic.main.fragment_players.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,13 +20,19 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FragmentWelcome.newInstance] factory method to
+ * Use the [FragmentPlayers.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FragmentWelcome : Fragment() {
+class FragmentPlayers : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var viewModel: PlayerViewModel
+
+    private lateinit var myAdapter: PlayerAdapter
+    private lateinit var myLayoutManager: LinearLayoutManager
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,27 +46,31 @@ class FragmentWelcome : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+
+        viewModel = ViewModelProvider(requireActivity()).get(PlayerViewModel::class.java)
+        viewModel.clear()
+
+        viewModel.getFullPlayerList()
+
+        myAdapter = PlayerAdapter(viewModel.allPlayers)
+        myLayoutManager = LinearLayoutManager(context)
+
+        viewModel.allPlayers.observe(
+            viewLifecycleOwner,
+            androidx.lifecycle.Observer {
+                myAdapter.notifyDataSetChanged()
+                progressBarPlayers.visibility = View.INVISIBLE
+            })
+
+        return inflater.inflate(R.layout.fragment_players, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        goToPlayers.setOnClickListener {
-            view -> view.findNavController().navigate(R.id.action_fragmentWelcome_to_fragmentPlayers)
-        }
-
-        goToGames.setOnClickListener {
-            view -> view.findNavController().navigate(R.id.action_fragmentWelcome_to_fragmentGames)
-        }
-
-        goToClubs.setOnClickListener {
-            view -> view.findNavController().navigate(R.id.action_fragmentWelcome_to_fragmentTeams)
-        }
-
-        goToFavTeams.setOnClickListener{
-            view -> view.findNavController().navigate(R.id.action_fragmentWelcome_to_fragmentFavoriteTeams)
+        recyclerView = playersTable.apply {
+            this.layoutManager = myLayoutManager
+            this.adapter = myAdapter
         }
     }
 
@@ -67,12 +81,12 @@ class FragmentWelcome : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentWelcome.
+         * @return A new instance of fragment FragmentPlayers.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FragmentWelcome().apply {
+            FragmentPlayers().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
