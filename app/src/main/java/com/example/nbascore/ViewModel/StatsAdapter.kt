a98b49f52783e11e15db5815b5f8373872501538
Adam Wolszleger <43927273+sylfero@users.noreply.github.com>
2021-02-03 22:50:35 +0100

    Bug fix

--- app/src/main/java/com/example/nbascore/ViewModel/StatsAdapter.kt
+++ app/src/main/java/com/example/nbascore/ViewModel/StatsAdapter.kt
@@ -1,22 +1,17 @@
 package com.example.nbascore.ViewModel
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
-import com.example.nbascore.Model.DataSource
 import com.example.nbascore.Model.Entities.Stats
 import com.example.nbascore.R
-import com.google.android.material.internal.ContextUtils
 import com.google.android.material.internal.ContextUtils.*
+import java.lang.Exception
 
 class StatsAdapter(var stats: LiveData<ArrayList<Stats>>) : RecyclerView.Adapter<StatsAdapter.StatsHolder>() {
     inner class StatsHolder(view: View): RecyclerView.ViewHolder(view)
@@ -32,14 +27,14 @@ class StatsAdapter(var stats: LiveData<ArrayList<Stats>>) : RecyclerView.Adapter
         var name = holder.itemView.findViewById<TextView>(R.id.playerStatsName)
         var pos = holder.itemView.findViewById<TextView>(R.id.playerPosition)
         var gametime = holder.itemView.findViewById<TextView>(R.id.playerGameTime)
-        var points = holder.itemView.findViewWithTag<TextView>(R.id.playerPoints)
-        var rebounds = holder.itemView.findViewWithTag<TextView>(R.id.playerRebounds)
-
+        var points = holder.itemView.findViewById<TextView>(R.id.playerPoints)
+        var rebounds = holder.itemView.findViewById<TextView>(R.id.playerRebounds)
+        
         name.text = stats.value?.get(position)?.player?.last_name
         pos.text = stats.value?.get(position)?.player?.position
         gametime.text = stats.value?.get(position)?.min
-        //rebounds.text = stats.value?.get(position)?.dreb.toString()
-        //points.text = stats.value?.get(position)?.pts.toString()
+        rebounds.text = stats.value?.get(position)?.dreb.toString()
+        points.text = stats.value?.get(position)?.pts.toString()
 
         Log.d("TEST", stats.value?.get(position)?.reb.toString() + " " + stats.value?.get(position)?.pts.toString())
     }
